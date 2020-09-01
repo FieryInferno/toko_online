5660f79a1891ea65487c9a1c70bd2f5fc2205038
= <riginlining2@gmail.com>
2020-09-01 20:43:04 +0700

    Fixed Footer :fire:

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -10,6 +10,14 @@
             $this->load->view('templates/footer');
         }
 
+        public function error_404()
+        {
+            $this->load->view('templates/header');
+            $this->load->view('templates/sidebar');
+            $this->load->view('error_404');
+            $this->load->view('templates/footer');
+        }
+
 
     }
 
